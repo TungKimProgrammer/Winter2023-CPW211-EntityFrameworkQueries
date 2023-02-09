fba11f71c9ccb545211f514fab22e8693d79b0cd
TUNG N KIM <Tungkims@gmail.com>
2023-02-08 22:47:06 -0600

    Added example to select all vendors

--- EntityFrameworkQueries/Form1.cs
+++ EntityFrameworkQueries/Form1.cs
@@ -1,3 +1,6 @@
+using EntityFrameworkQueries.Data;
+using EntityFrameworkQueries.Models;
+
 namespace EntityFrameworkQueries
 {
     public partial class Form1 : Form
@@ -5,6 +8,18 @@ namespace EntityFrameworkQueries
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnSelectAllVendors_Click(object sender, EventArgs e)
+        {
+            using APContext dbContext = new APContext();
+
+            // LINQ (Language Intergrated Query) method syntax
+            List<Vendor> vendorList = dbContext.Vendors.ToList();
+
+            // LINQ query syntax
+            List<Vendor> vendorList2 = (from v in dbContext.Vendors
+                                        select v).ToList();
         }
     }
 }
